3832bb6d75b344962471b12b136211a0916560b9
Kane Cheshire <iKane@MacBook-Pro.lan>
2015-01-30 17:21:30 +0000

    Initial Commit

---
+++ BLETEST/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BLETEST
+//
+//  Created by Kane Cheshire on 30/01/2015.
+//  Copyright (c) 2015 Kane Cheshire. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
